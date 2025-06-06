2142866e5d7213549e912c0253a3a14df98b91ae
simplyObare <josephatowiny@gmail.com>
2025-06-06 05:07:45 +0300

    creating serializers

--- Ecommerce_DRF/product/urls.py
+++ Ecommerce_DRF/product/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import product_list
 
 
-urlpatterns = []
+urlpatterns = [
+    path("products/", product_list, name="product-list"),
+]
--- Ecommerce_DRF/product/views.py
+++ Ecommerce_DRF/product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
