50453ae3d92ee16af1265dcf2d391aa6b52ca28d
simplyObare <josephatowiny@gmail.com>
2025-06-07 02:59:51 +0300

    APIView CRUD Operations Django Rest Framework

--- Ecommerce_DRF/product/views.py
+++ Ecommerce_DRF/product/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework import status
 from .simple_serializers import Message
 
 
@@ -34,6 +35,14 @@ class ProductListView(APIView):
 
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ProductDetailsView(APIView):
 
@@ -45,3 +54,24 @@ class ProductDetailsView(APIView):
 
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+
+    def put(self, request, product_id):
+        try:
+            product = Product.objects.get(product_id=product_id)
+        except Product.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, product_id):
+        try:
+            product = Product.objects.get(product_id=product_id)
+        except Product.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
