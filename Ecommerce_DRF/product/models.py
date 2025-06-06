bbf233671da36a462c88a424710c7282623b2857
simplyObare <josephatowiny@gmail.com>
2025-06-06 04:22:30 +0300

    creating models

---
+++ Ecommerce_DRF/product/models.py
@@ -0,0 +1,13 @@
+import uuid
+from django.db import models
+
+
+class Product(models.Model):
+    product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
