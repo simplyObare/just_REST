4d18f22c82335c9107e48a08ad98b3d5aa2199a5
simplyObare <josephatowiny@gmail.com>
2025-06-10 05:37:03 +0300

    Authentications in Django Rest Framework

--- Ecommerce_DRF/m_commerce/settings.py
+++ Ecommerce_DRF/m_commerce/settings.py
@@ -114,4 +114,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 2,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
