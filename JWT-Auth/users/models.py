70b31edad1c932320da03d5fdc04773959aec9bf
simplyObare <josephatowiny@gmail.com>
2025-06-12 19:46:58 +0300

    creating views and serializing the models

--- JWT-Auth/users/models.py
+++ JWT-Auth/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    username = None
+    password = models.CharField(max_length=150, null=True, blank=True)
+    
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
--- JWT-Auth/users/urls.py
+++ JWT-Auth/users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import RegisterView
 
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", RegisterView.as_view()),
+]
